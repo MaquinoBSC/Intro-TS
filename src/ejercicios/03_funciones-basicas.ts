5b05ac5acba46924d9219e93ddceea89198886ad
MaquinoBSC <martinare.ipn@gmail.com>
2022-01-12 14:47:16 -0600

    Objetos como parametos en funciones

--- src/ejercicios/03_funciones-basicas.ts
+++ src/ejercicios/03_funciones-basicas.ts
@@ -12,3 +12,26 @@ function multiplicar(numero: number, base: number, otroNumero?: number){
 
 const resultado= multiplicar(10, 20);
 console.log(resultado);
+
+
+interface PersonajeLOR {
+    nombre: string;
+    pv: number;
+    mostrarHp: ()=> void;
+}
+
+function curarPersonaje(personaje: PersonajeLOR, curarX: number): void{
+    personaje.pv+= curarX;
+}
+
+const nuevoPersonaje01: PersonajeLOR= {
+    nombre: 'Strider',
+    pv: 50,
+    mostrarHp(){
+        console.log('Puntos de vida: ', this.pv);
+        
+    }
+}
+
+curarPersonaje( nuevoPersonaje01, 20 );
+nuevoPersonaje01.mostrarHp();
