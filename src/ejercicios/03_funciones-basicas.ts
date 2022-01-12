331d1c04e7442e20583a38148f0cbe0c976a9d66
MaquinoBSC <martinare.ipn@gmail.com>
2022-01-11 22:46:09 -0600

    Funciones basicas

---
+++ src/ejercicios/03_funciones-basicas.ts
@@ -0,0 +1,14 @@
+function sumar(a: number, b: number): number{
+    return a+b;
+};
+
+const sumarFlecha= (a: number, b: number): number => {
+    return a+b;
+}
+
+function multiplicar(numero: number, base: number, otroNumero?: number){
+    return numero*base;
+}
+
+const resultado= multiplicar(10, 20);
+console.log(resultado);
